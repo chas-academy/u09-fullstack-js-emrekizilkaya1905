b94bf4f27a5fbb43b5b3b489d819786aedacf719
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-24 13:46:11 +0200

    create pages and redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Cities from "./pages/Cities";
 
 export default function App() {
-  return <h1 className="text-3xl text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/cities" element={<Cities />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/Cities.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cities = () => {
+  return <div>City</div>;
+};
+
+export default Cities;
