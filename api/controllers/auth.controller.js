c65ff60dc9365b58c257d6c8d32e24c8f0de4c61
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-24 20:37:15 +0200

    Added hashed password and change the controllers name.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../Models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (request, response) => {
   const { username, email, password } = request.body;
@@ -12,7 +13,12 @@ export const signup = async (request, response) => {
   ) {
     return response.status(400).json({ message: "All fields are required" });
   }
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  response.json("Sign up succesfull");
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    response.json("Sign up succesfull");
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
 };
