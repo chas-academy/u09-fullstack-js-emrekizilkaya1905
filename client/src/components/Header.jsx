8ba1d103595413140f5eff3a0479297b342429c5
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-25 13:05:48 +0200

    Change the route sign in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/signin">
+        <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
