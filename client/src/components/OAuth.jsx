5c6d22f388f614c81bc180b2f98e864734b6d7d0
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-27 12:10:29 +0200

    Fix the google auth ai

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+import React from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+
+export default function OAuth() {
+  return (
+    <Button type="button" gradientDuoTone="pinkToOrange" outline>
+      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+      Continue With Google
+    </Button>
+  );
+}
