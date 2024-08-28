6af1f9351f01a774b7a314b65ee29f57ea43d346
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-29 00:19:59 +0200

    useefffect  is working.

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,12 @@
 import { Button, TextInput } from "flowbite-react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -13,13 +14,30 @@ export default function DashProfile() {
       setImageFileUrl(URL.createObjectURL(file));
     }
   };
-  console.log(imageFile, imageFileUrl);
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    console.log("Uploading image");
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <input type="file" accept="*/image" onChange={handleImageChange} />
-        <div className="w-32 h-32 self-center cursor-pointer">
+        <input
+          type="file"
+          accept="*/image"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="user"
