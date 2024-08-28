d8b9e53c4f5058904a4a65d0e0614483e0d01b8b
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-28 20:29:52 +0200

    I can get url

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl);
+  }, [location.search]);
+  return (
+    <div>
+      <div className="">{/* Sidebar*/}</div>
+      {/* Profile*/}
+    </div>
+  );
 }
