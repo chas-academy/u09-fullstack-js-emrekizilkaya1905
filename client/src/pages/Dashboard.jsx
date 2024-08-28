b7e9262a26848f22b2392760e3b11251a9a9ee31
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-28 21:50:14 +0200

    Dashsidebar ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -7,12 +9,17 @@ export default function Dashboard() {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
-    console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
   }, [location.search]);
   return (
-    <div>
-      <div className="">{/* Sidebar*/}</div>
-      {/* Profile*/}
+    <div className="min h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar*/}
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
     </div>
   );
 }
