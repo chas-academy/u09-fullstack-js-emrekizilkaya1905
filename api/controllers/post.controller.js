b58d12b4702ca41ec51b7b5108390104de8222b7
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-09-09 19:54:21 +0200

    create and tested post api route

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,23 @@
+import Post from "../Models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const create = async (request, response, next) => {
+  if (!request.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post."));
+  }
+  if (!request.body.title || !request.body.content) {
+    return next(errorHandler(400, "Please provide all require the fields"));
+  }
+  const slug = request.body.title
+    .split("")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
+  const newPost = new Post({ ...request.body, slug, userId: request.user.id });
+  try {
+    const savedPost = await newPost.save();
+    response.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./Routes/user.route.js";
 import authRoutes from "./Routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import postRoutes from "./Routes/post.route.js";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 // Create a middleware for errors.
 app.use((error, request, response, next) => {
