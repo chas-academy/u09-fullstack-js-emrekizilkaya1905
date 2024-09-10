3aab8d1a3b3eecec4ed42fa368c792a383a89da4
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-09-11 00:59:47 +0200

    I can see posts which is created by admin.

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const DashPosts = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          `/api/post/getposts?userId=${currentUser._id}`
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {}
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
   return <div>Dashposts</div>;
 };
 
