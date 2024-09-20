9f52d41156e771a30fac32c64e5684113ba17348
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-09-20 16:16:36 +0200

    Edit Comment is done.

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -47,3 +47,24 @@ export const likeComment = async (request, response, next) => {
     next(error);
   }
 };
+export const editComment = async (request, response, next) => {
+  try {
+    const comment = await Comment.findById(request.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment can not found"));
+    }
+    if (comment.userId !== request.user.id && !request.user.isAdmin) {
+      return next(errorHandler(404, "You are not allowed to edit."));
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      request.params.commentId,
+      {
+        content: request.body.content,
+      },
+      { new: true }
+    );
+    response.status(200).json(editedComment);
+  } catch (error) {
+    next(error);
+  }
+};
